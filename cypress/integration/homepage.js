41f22d169ff4a90f06da36e69fcbf812a2c681e7
Kim Ward <kmewrd@outlook.com>
2022-04-26 10:59:46 -0500

    Add test checking for url data from fixture

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -10,4 +10,14 @@ describe('Homepage of URL Shortener site', () => {
       .contains('URL Shortener')
   })
 
+  it('should display all shortened urls from the server', () => {
+    cy.get('div[class="url"]:first')
+      .should('have.descendants', 'h3')
+      .and('contain', 'Awesome photo')
+      .and('have.descendants', 'a')
+      .and('contain', 'http://localhost:3001/useshorturl/1')
+      .and('have.descendants', 'p')
+      .and('contain', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+  })
+
 })
